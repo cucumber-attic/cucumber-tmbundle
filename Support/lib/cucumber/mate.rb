654b90b9dca62207156dda5d6b8fbd99b21b57d6
Charles Lowell <cowboyd@thefrontside.net>
2011-02-08 10:17:26 -0600

    don't use deprecated bundler api. use rspec 2 as default, fall back to rspec 1

--- Support/lib/cucumber/mate.rb
+++ Support/lib/cucumber/mate.rb
@@ -7,7 +7,7 @@ if ENV['TM_PROJECT_DIRECTORY']
   bundler_gemfile = File.join(ENV['TM_PROJECT_DIRECTORY'], 'Gemfile')
   if File.exists?(bundler_gemfile)
     bundle_path = (File.read(bundler_gemfile) =~ (/bundle_path[ (]+['"](.*?)['"]/) && $1) || ".bundle"
-    require File.join(ENV['TM_PROJECT_DIRECTORY'], bundle_path, "environment")
+    require 'bundler/setup'
   elsif File.directory?(rspec_rails_plugin)
     $LOAD_PATH.unshift(rspec_rails_plugin)
   elsif File.directory?(rspec_merb_gem)
@@ -20,7 +20,11 @@ if ENV['TM_PROJECT_DIRECTORY']
     $LOAD_PATH.unshift(rspec_lib)
   end
 end
-require 'spec'
+begin
+  require 'rspec'
+rescue LoadError
+  require 'spec'
+end
 
 $LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), '..')))
 require "cucumber/mate/feature_helper"
